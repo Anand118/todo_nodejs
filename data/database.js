ebecfb29bdb00cb5aa921f23ddb79efe968d962c
Anand Prakash <anandprakashj9155@gmail.com>
2025-06-05 22:13:41 +0530

    Something Changed

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URL, { dbName: "backendapi2", })
-    .then((c) => console.warn(`Database Connected`))
+    .then((c) => console.warn(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.warn(e));
 };
--- utils/features.js
+++ utils/features.js
@@ -3,8 +3,7 @@ import jwt from 'jsonwebtoken';
 
 export const sendCookie = (user, res, message, statusCode = 200) => {
     let token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-    console.warn(process.env.NODE_ENV)
-    console.warn(process.env.NODE_ENV === "Development" )
+    
     res.status(statusCode).cookie("token", token, {
         httpOnly: true,
         maxAge: 15 * 60 * 1000,
